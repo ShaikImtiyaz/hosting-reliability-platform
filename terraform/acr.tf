ada313ed5493c989420e3bcc2cf860261d015867
Shaik Mohammed Imtiyaz <imtiyazshaik.25492@gmail.com>
2026-07-12 17:38:14 +0530

    Milestone 4: Add Terraform foundation and provision Azure Resource Group

---
+++ terraform/acr.tf
@@ -0,0 +1,14 @@
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  sku           = "Basic"
+  admin_enabled = true
+
+  tags = {
+    Project     = "HostingReliabilityPlatform"
+    Environment = "Development"
+    Owner       = "Imtiyaz"
+  }
+}
