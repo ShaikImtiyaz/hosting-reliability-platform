35000dffcbc7f7ba182713a3253d646d3b02c3e0
Shaik Mohammed Imtiyaz <imtiyazshaik.25492@gmail.com>
2026-07-12 16:00:25 +0530

    Milestone 4: Add Terraform foundation for Azure infrastructure

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,9 @@
+subscription_id = "15122a17-4555-458c-829b-cf14a3dc0dcb"
+
+location = "Central India"
+
+resource_group_name = "rg-hosting-reliability-dev"
+
+acr_name = "acrhostingrel001"
+
+aks_name = "aks-hosting-reliability"
---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+}
+
+variable "acr_name" {
+  description = "Azure Container Registry Name"
+  type        = string
+}
+
+variable "aks_name" {
+  description = "AKS Cluster Name"
+  type        = string
+}
